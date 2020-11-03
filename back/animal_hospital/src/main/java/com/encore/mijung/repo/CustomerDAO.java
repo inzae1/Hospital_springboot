b17f552ed738fe344c3a99552e496ad654717c95
JeeHyesoo <hyesoo.jee@gmail.com>
2020-11-03 11:07:29 +0900

    customer series

--- back/animal_hospital/src/main/java/com/encore/mijung/repo/CustomerDAO.java
+++ back/animal_hospital/src/main/java/com/encore/mijung/repo/CustomerDAO.java
@@ -3,10 +3,11 @@ package com.encore.mijung.repo;
 import com.encore.mijung.domain.Customer;
 
 public interface CustomerDAO {
-	public void register(Customer customer) throws Exception;
+	public boolean register(Customer customer) throws Exception;
 	public Customer login(Customer customer) throws Exception;
-	public void dropCustomer(String custId) throws Exception;
+	public boolean dropCustomer(String custId) throws Exception;
 	public int idCheck(String custId) throws Exception;
 	public Customer findId(Customer customer) throws Exception;
 	public Customer findPassword(Customer customer) throws Exception;
+	public boolean changePassword(Customer customer) throws Exception;
 }
